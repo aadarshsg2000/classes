66a2256d9a3cd17fe27545252d64f9836ee82130
Aadarsh sg <sgaadarshinfotech@gmail.com>
2020-03-18 11:59:51 +0530

    Pushing after a pull

---
+++ classes.py
@@ -0,0 +1,36 @@
+#adding class for learners and mentors
+class tech_learners_and_mentors:
+    def __init__(self,name,ids,aot,interest,expertise):
+        #aot means area of interest
+        self.name=name
+        self.ids=ids #ID
+        self.aot=aot
+        self.interest=interest
+        self.expertise=expertise
+    #adding the stack value with self init
+    def addStacks(self,value):
+        self.value
+        value.interest=interest
+        value.expertise=expertise
+   #assigning for mentor and learner
+    def setMentorOrLearner(self,setval):
+       setval=['Expertise','Learner']
+       print("If it is interest",str(strval[1]))
+      #if expertise they will be mentor
+       print("If it is expertise",set(str(setval)))
+    def setAvailableTime(self,person):
+        if person=='Mentor':
+            return 'Time is available'
+        else:
+            return set(setAvailableTime)
+    #checking for available mentors
+    def getMentor(self,check,uncheck):
+        print('Available mentors!')
+        return self.person
+    #if mentor found it shows unless mentor found
+    
+        
+    
+        
+        
+
